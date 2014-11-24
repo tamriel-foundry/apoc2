64452e0c65ce6fa0f7e5653e7746f1966133b9b8
Andrew <aaclayton@gmail.com>
2014-11-24 12:59:11 -0800

    TinyMCE padding fix

--- functions.php
+++ functions.php
@@ -140,7 +140,7 @@ function apoc_mce_options( $init ) {
 	
 	// TinyMce initialization options
 	if( !is_admin() )
-			$init['content_css']				= $stylesheet . 'editor-content.css?v=1.2';
+			$init['content_css']				= $stylesheet . 'editor-content.css?v=1.3';
 	$init['wordpress_adv_hidden'] 				= false;
 	$init['height']								= 250;
 	$init['theme_advanced_resizing_use_cookie'] = false;
